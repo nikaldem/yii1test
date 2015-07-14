cedc38832a3c0d0fbd9874aad9b3dc2f382de755
nikaldem <nikdem@yandex.ru>
2015-07-14 10:04:21 +0300

    Установка yii1 и первые пробы

---
+++ protected/views/page/admin.php
@@ -0,0 +1,50 @@
+<?php
+/* @var $this PageController */
+/* @var $model Page */
+
+$this->breadcrumbs=array(
+	'Pages'=>array('index'),
+	'Manage',
+);
+
+$this->menu=array(
+	array('label'=>'List Page', 'url'=>array('index')),
+	array('label'=>'Create Page', 'url'=>array('create')),
+);
+
+Yii::app()->clientScript->registerScript('search', "
+$('.search-button').click(function(){
+	$('.search-form').toggle();
+	return false;
+});
+$('.search-form form').submit(function(){
+	$('#page-grid').yiiGridView('update', {
+		data: $(this).serialize()
+	});
+	return false;
+});
+");
+?>
+
+<h1>Управление страницами</h1>
+
+<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
+<div class="search-form" style="display:none">
+<?php $this->renderPartial('_search',array(
+	'model'=>$model,
+)); ?>
+</div><!-- search-form -->
+
+<?php $this->widget('zii.widgets.grid.CGridView', array(
+	'id'=>'page-grid',
+	'dataProvider'=>$model->search(),
+	'filter'=>$model,
+	'columns'=>array(
+		'id',
+		'title',
+		'text',
+		array(
+			'class'=>'CButtonColumn',
+		),
+	),
+)); ?>
